d62cd55389978a02d214cbabd31017d326e1b38a
Patrick Mutwiri <patwiri@gmail.com>
2020-05-08 13:46:43 +0300

    submit

--- app/src/main/java/com/mutwiri/sectioniii2activities/GetInput.java
+++ app/src/main/java/com/mutwiri/sectioniii2activities/GetInput.java
@@ -1,5 +1,7 @@
 package com.mutwiri.sectioniii2activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,15 +10,49 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class GetInput extends AppCompatActivity {
+public class GetInput extends AppCompatActivity implements View.OnClickListener {
+    protected EditText fnumber;
+    protected EditText snumber;
+    protected float number1, number2;
+    protected double total;
+    protected String myTag="Errors ",resultsDisplay = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.getinput);
+        setContentView(R.layout.activity_get_input);
 
+        Button calc=(Button)findViewById(R.id.button2);
+        calc.setOnClickListener(this);
     }
+    public void onClick(View view) {
+        Intent data = new Intent();
 
+        fnumber = fnumber.findViewById(R.id.entrynumber1);
+        snumber = snumber.findViewById(R.id.entrynumber2);
+        resultsDisplay = "";
+
+        if(fnumber != null) {
+            try {
+                number1 = Float.parseFloat(fnumber.getText().toString());
+                number2 = Float.parseFloat(snumber.getText().toString());
+                total = (number1*1.0)+(number2*1.0);
+
+                resultsDisplay = String.format("\nThe sum of %.2f and %.2f is %.2f ",number1,number2,total);
+            } catch (Exception e) {
+                Log.v(myTag,e.getMessage());
+                resultsDisplay = e.getMessage();
+            }
+        }
+
+        data.setData(Uri.parse(resultsDisplay));
+        setResult(RESULT_OK, data);
+        finish();
+    }
 }
--- app/src/main/java/com/mutwiri/sectioniii2activities/MainActivity.java
+++ app/src/main/java/com/mutwiri/sectioniii2activities/MainActivity.java
@@ -4,46 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity  extends AppCompatActivity implements View.OnClickListener {
 
-    protected EditText fnumber;
-    protected EditText snumber;
-    protected TextView results;
     protected Button getInput;
-    protected Button sum;
-
-    protected float number1, number2, total;
-    protected String stdName = "", myTag="Errors ",resultsDisplay = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getInput = findViewById(R.id.button);
-        getInput.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // load input activity
-                Intent getInput = new Intent("com.mutwiri.sectioniii2activities.GetInput");
-                startActivityForResult(getInput,1);
-            }
+        getInput.setOnClickListener(this);
+    }
 
-            public void onActivityResult(int requestCode, int resultCode, Intent data) {
-                if (requestCode == 1) {
-                    if (resultCode == RESULT_OK) {
-                        /*Toast.makeText(getInput, data.getData().toString(),
-                                Toast.LENGTH_LONG).show();*/
-                    }
-                }
+    public void onClick(View v) {
+        // load input activity
+        Intent getInput = new Intent("com.mutwiri.sectioniii2activities.GetInput");
+        startActivityForResult(getInput,1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_LONG).show();
             }
-        });
-        //
+        }
     }
 }
